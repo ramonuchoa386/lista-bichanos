8c56dc76309c8efdb080a9cca43299bc20c2f25d
Ramon Pinho <ramonuchoa386@gmail.com>
2022-06-16 19:27:48 -0300

    feat: update project to new arch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { theme } from "./styles/theme";
+import GlobalStyles from "./styles/global";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
