f7252cc124a9101f4400661767bc1d3890ed1556
Ramon Pinho <ramonuchoa386@gmail.com>
2022-06-16 19:32:20 -0300

    feat: update react render

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 import { theme } from "./styles/theme";
 import GlobalStyles from "./styles/global";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+
+root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
