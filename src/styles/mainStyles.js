53152f4fb5a13b5373e6adad35dc682ec6fae7dd
Ramon Pinho <ramonuchoa386@gmail.com>
2022-06-17 21:30:46 -0300

    feat: fix main wrapper width and overflow

--- src/styles/mainStyles.js
+++ src/styles/mainStyles.js
@@ -2,16 +2,14 @@ import styled from "styled-components";
 
 export const AppWrapper = styled.div`
   display: flex;
-  width: 100vw;
+  width: 100%;
   height: 100vh;
 `;
 
 export const Main = styled.main`
   background-color: ${(props) => props.theme.colors.grey};
-
-  @media only screen and (max-width: 520px) {
-    diplay: flex;
-    flex-direction: column;
-    overflow: scroll;
-  }
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
 `;
