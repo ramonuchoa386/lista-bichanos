d3705b298db8e5e3f63366067682d05949c4ae3e
Ramon Pinho <ramonuchoa386@gmail.com>
2022-06-24 23:32:04 -0300

    feat: create template

---
+++ src/components/templates/defaultPage/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+export const AppWrapper = styled.div`
+	display: flex;
+	width: 100%;
+	height: 100vh;
+`
+
+export const Main = styled.main`
+	background-color: ${(props) => props.theme.colors.grey};
+	overflow-y: scroll;
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+`
---
+++ src/components/templates/index.ts
@@ -0,0 +1,3 @@
+import DefaultTemplate from "./defaultPage"
+
+export { DefaultTemplate }
