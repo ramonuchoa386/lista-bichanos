fd351ea76d03b1cb9e52013165ae054ac94edfbb
Ramon Pinho <ramonuchoa386@gmail.com>
2022-06-25 23:10:04 -0300

    feat: prettier format

--- src/components/atoms/heading/styles.ts
+++ src/components/atoms/heading/styles.ts
@@ -1,5 +1,5 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 
-export const H1 = styled.h1``
-export const H2 = styled.h2``
-export const H3 = styled.h3``
+export const H1 = styled.h1``;
+export const H2 = styled.h2``;
+export const H3 = styled.h3``;
