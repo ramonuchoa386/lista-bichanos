835cb8713c5e9148da0ef1730827a0bb0adf0519
Ramon Pinho <ramonuchoa386@gmail.com>
2022-06-25 23:08:17 -0300

    feat: create wrapper component

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,8 +1,9 @@
-import Heading from "./heading"
-import Button from "./button"
-import Icon from "./icon"
-import Img from "./img"
-import Link from "./link"
-import Text from "./text"
+import Heading from './heading';
+import Button from './button';
+import Icon from './icon';
+import Img from './img';
+import Link from './link';
+import Text from './text';
+import Wrapper from './wrapper';
 
-export { Heading, Button, Icon, Img, Link, Text }
+export { Heading, Button, Icon, Img, Link, Text, Wrapper };
