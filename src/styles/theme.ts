781819700963193f741a8b8367bd1bde5f31b596
Ramon Pinho <ramonuchoa386@gmail.com>
2022-06-25 23:07:28 -0300

    feat: add breakpoint to theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,33 +1,35 @@
-interface IColors {
-	[key: string]: string
-}
-
-interface IFontSizes {
-	[key: string]: string
+interface IThemeProps {
+	[key: string]: string;
 }
 
 export interface IThemeType {
-	colors: IColors
-	fontSizes: IFontSizes
+	colors: IThemeProps;
+	fontSizes: IThemeProps;
+	breakpoints: IThemeProps;
 }
 
 const theme = {
 	colors: {
-		lightGrey: "#c2c7ca",
-		grey: "#ebeff3",
-		paleGrey: "#abb3b6",
-		darkGrey: "#545454",
-		slateGrey: "#a7b9ce",
-		starRated: "#40b186",
-		lightBlue: "#4480c4",
-		blue: "#2e578b",
-		paleBlue: "#3767a2",
+		lightGrey: '#c2c7ca',
+		grey: '#ebeff3',
+		paleGrey: '#abb3b6',
+		darkGrey: '#545454',
+		slateGrey: '#a7b9ce',
+		starRated: '#40b186',
+		lightBlue: '#4480c4',
+		blue: '#2e578b',
+		paleBlue: '#3767a2',
 	},
 	fontSizes: {
-		small: ".625rem",
-		regular: "1rem",
-		big: "1.5rem",
+		small: '.625rem',
+		regular: '1rem',
+		big: '1.5rem',
 	},
-}
+	breakpoints: {
+		mobile: '320px',
+		tablet: '768px',
+		desktop: '1280px',
+	},
+};
 
-export default theme
+export default theme;
