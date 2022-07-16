19985b0e071957ca4a6b6c4739d9ee08be44b981
Ramon Pinho <ramonuchoa386@gmail.com>
2022-07-16 00:08:11 -0300

    feat: remove unused component

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,7 +1,6 @@
-import Navigation from './navigation';
 import Profile from './profile';
 import Rating from './rating';
 import Result from './result';
 import SearchInfo from './searchInfo';
 
-export { Navigation, Profile, Rating, Result, SearchInfo };
+export { Profile, Rating, Result, SearchInfo };
