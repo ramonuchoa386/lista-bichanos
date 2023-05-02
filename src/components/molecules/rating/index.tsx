253e474d94a6fa44858e1d28e500c2093cf74696
Ramon Pinho <ramonuchoa386@gmail.com>
2023-05-02 00:03:06 -0300

    feat: refactor rating component

--- src/components/molecules/rating/index.tsx
+++ src/components/molecules/rating/index.tsx
@@ -1,21 +1,22 @@
-import * as React from 'react';
-import { RatingWrapper, RatingIcon } from './styles';
+import React, { FunctionComponent } from 'react';
+import { MdStarBorder, MdStar } from 'react-icons/md';
+import { RatingWrapper } from './styles';
 
-interface IRating {
-	stars: Array<unknown>;
-	icon: string;
+export interface IRating {
+	stars: number;
 }
 
-function Rating(props: IRating) {
-	const { stars, icon } = props;
+const Rating: FunctionComponent<IRating> = (props) => {
+	const { stars } = props;
 
 	return (
 		<RatingWrapper>
-			{stars.map((star) => (
-				<RatingIcon key={`star-${star}-${Math.random()}`} icon={icon} />
-			))}
+			{new Array(5)
+				.fill(0)
+				.fill(1, 0, stars)
+				.map((star) => (star === 1 ? <MdStar /> : <MdStarBorder />))}
 		</RatingWrapper>
 	);
-}
+};
 
 export default Rating;
