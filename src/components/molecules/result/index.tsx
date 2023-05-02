b91d29177a1ff54f5aff448e6d9b264c03bf2027
Ramon Pinho <ramonuchoa386@gmail.com>
2023-05-02 00:04:17 -0300

    feat: refactor result component

--- src/components/molecules/result/index.tsx
+++ src/components/molecules/result/index.tsx
@@ -1,34 +1,75 @@
-import * as React from 'react';
+/* eslint-disable camelcase */
+import React, { FunctionComponent, useEffect, useState } from 'react';
 import { SearchInfo } from '..';
 import { ResultsWrapper, ResultThumbnail } from './styles';
+import useFetchBreedDetails from '../../../hooks/useFetchBreedDetails';
+import { Loader } from '../../atoms';
 
 interface IResult {
-	imageSrc: string;
-	alterText: string;
-	breedName: string;
-	breedDescription: string;
-	temperaments: Array<any>;
+	breedId: string;
 }
 
-function Result(props: IResult) {
-	const { imageSrc, alterText, breedName, breedDescription, temperaments } =
-		props;
+const Result: FunctionComponent<IResult> = (props) => {
+	const { breedId } = props;
+	const { loading, data, error } = useFetchBreedDetails(breedId);
+	const [breedName, setBreedName] = useState<string>('');
+	const [breedDesc, setBreedDesc] = useState<string>('');
+	const [affectionLevel, setAffectionLevel] = useState<number>(0);
+	const [adapt, setAdapt] = useState<number>(0);
+	const [childFriendly, setChildFriendly] = useState<number>(0);
+	const [dogFriendly, setDogFriendly] = useState<number>(0);
+
+	useEffect(() => {
+		if (!loading && data !== undefined && error === undefined) {
+			const {
+				name,
+				description,
+				affection_level,
+				adaptability,
+				child_friendly,
+				dog_friendly,
+			} = data;
+
+			setBreedName(name);
+			setBreedDesc(description);
+			setAffectionLevel(affection_level);
+			setAdapt(adaptability);
+			setChildFriendly(child_friendly);
+			setDogFriendly(dog_friendly);
+		}
+	}, [loading, data, error, breedId]);
+
+	if (loading) {
+		return <Loader showLoader={loading} />;
+	}
 
 	return (
 		<ResultsWrapper>
-			<ResultThumbnail
-				src={imageSrc}
-				width="200px"
-				height="200px"
-				alt={alterText}
-			/>
+			<ResultThumbnail src="" width="200px" height="200px" alt="" />
 			<SearchInfo
 				breedName={breedName}
-				temperaments={temperaments}
-				breedDescription={breedDescription}
+				temperaments={[
+					{
+						ratingType: 'Affection Level',
+						stars: affectionLevel,
+					},
+					{
+						ratingType: 'Adaptability',
+						stars: adapt,
+					},
+					{
+						ratingType: 'Child Friendly',
+						stars: childFriendly,
+					},
+					{
+						ratingType: 'Dog Friendly',
+						stars: dogFriendly,
+					},
+				]}
+				breedDescription={breedDesc}
 			/>
 		</ResultsWrapper>
 	);
-}
+};
 
 export default Result;
